9e61ec0f147a0c761f920d5aaef92c9593c1dd6e
谷崎公紀 <marutto8@gmail.com>
2020-03-14 13:35:14 +0900

    add index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
   def new
     @post = Post.new # フォーム用の空のインスタンスを生成する。
   end
   
   def create
-    @post = Post.new(post_params) # ストロングパラメータを引数に
-    @post.save # saveをしてデータベースに保存する。
-    redirect_to @post, notice: '投稿を保存しました' # showページにリダイレクト
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿を登録しました。"
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,7 +25,6 @@ class PostsController < ApplicationController
   private
 
   def post_params # ストロングパラメータを定義する
-    params.require(:post).permit(:caption)
     params.require(:post).permit(:caption, :image)
   end
 
