b2531e36bdfdfeed228279cb10183ce4b233120a
谷崎公紀 <marutto8@gmail.com>
2020-03-15 15:34:22 +0900

    add serch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
-module ApplicationHelper
-
-  def user_icon _link(class_name)
-    if user_signed_in? 
-      link_to user_path(current_user), class: class_name do 
-        <i class="far fa-user"></i>
-      end 
-    else 
-      link_to new_user_session_path, class: class_name do 
-        <i class="far fa-user"></i>
-      end 
-    end 
+  module ApplicationHelper
+    def user_icon_link(class_name)
+      if user_signed_in?
+        link_to user_path(current_user), class: class_name do
+          content_tag(:i, "", class: "far fa-user")
+        end
+      else
+        link_to new_user_session_path, class: class_name do
+          content_tag(:i, "", class: "far fa-user")
+        end
+      end
+    end
   end
-end
